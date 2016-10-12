43d5db36497418172653099fa8beee9f0f0361f6
Eric Abell <crucalculus@gmail.com>
2016-10-11 21:31:00 -0500

    add ability to put datetimestamp in document

--- insert_one.js
+++ insert_one.js
@@ -4,7 +4,9 @@ MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
     if (err) throw err;
     console.log("Successfully connected to the database!");
 
-    db.collection('movies').insertOne({'name': 'Star Wars'});
+    var result = db.collection('movies').insertOne({'name': 'Star Wars', 'date': new Date()});
+
+    console.log(result);
 
     db.close();
 })
